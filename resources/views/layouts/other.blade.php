deddb45513ff492d5147ecfd826785e3146d7b3f
vagrant <vagrant@homestead>
2020-06-04 09:57:02 +0000

    底部修改

--- resources/views/layouts/other.blade.php
+++ resources/views/layouts/other.blade.php
@@ -82,7 +82,8 @@
                     <div class="contact">
                         <a href="javascript:void(0)" class="github" target="_blank"><i class="fa fa-github"></i></a>
                         <a href="https://wpa.qq.com/msgrd?v=3&uin=1683972097&site=qq&menu=yes" class="qq" target="_blank" title="1683972097"><i class="fa fa-qq"></i></a>
-                        <a href="https://mail.qq.com/cgi-bin/qm_share?t=qm_mailme&email=gbiysbG0tbWyuMHw8K-i7uw" class="email" target="_blank" title="1683972097@qq.com"><i class="fa fa-envelope"></i></a>
+<!--                         <a href="https://mail.qq.com/cgi-bin/qm_share?t=qm_mailme&email=gbiysbG0tbWyuMHw8K-i7uw" class="email" target="_blank" title="1683972097@qq.com"><i class="fa fa-envelope"></i></a> -->
+                        <a href="{{ route('about.index') }}" class="email" target="_blank" title="1683972097@qq.com"><i class="fa fa-envelope"></i></a>
                         <a href="{{ route('about.index') }}" class="weixin"><i class="fa fa-weixin"></i></a>
                     </div>
                     <p class="mt05">
